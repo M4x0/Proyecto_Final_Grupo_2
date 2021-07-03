8c74519ed76a3e20103ff72bc90363f934586f7c
M4x0 <86454453+M4x0@users.noreply.github.com>
2021-07-03 18:28:42 -0500

    Update Trabajo_Final_Grupo_2.py

--- Trabajo_Final_Grupo_2.py
+++ Trabajo_Final_Grupo_2.py
@@ -1,1 +1,11 @@
-print("Hola Mundo");
+#Ejercicio del trabajo final del Grupo 2
+def capturar():
+    global lista;
+    lista = [];
+    print ("¿Cuantos elementos va a tener la lista?");
+    n = int(input());
+    for i in range(0,n):
+        print("Ingresa el elemento del índice ", i);
+        elemento = input();
+        lista.insert(i,elemento);
+capturar();
